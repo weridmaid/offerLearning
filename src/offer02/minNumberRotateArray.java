b025749bb13a11cf27155cfbf5af20c78c8772c4
Sy <370418846@qq.com>
2017-08-05 21:26:26 +0800

    旋转数组查找最小数

--- src/offer02/minNumberRotateArray.java
+++ src/offer02/minNumberRotateArray.java
@@ -22,12 +22,40 @@ public class minNumberRotateArray {
            }
           return minNum;
     }
+    //用二分查找法实现
+    public int minNumberInRotate(int [] array,int length) {
+
+        //异常处理
+        if (array==null||length<=0){
+            return 0;
+        }
+        int start=0;
+        int end=length-1;
+        int indexmid=start;
+        while (array[start]>=array[end]){
+            if (end-start==1){
+                indexmid=end;
+                break;
+            }
+            indexmid=(start+end)/2;
+            if(array[start]<array[indexmid]){
+                start=indexmid;
+            }
+            else if(array[end]>array[indexmid]){
+                end=indexmid;
+            }
+        }
+        return array[indexmid];
+    }
 
     public static void main(String[] args) {
         int[] list={4,5,6,7,2,3};
         minNumberRotateArray test=new minNumberRotateArray();
-        int minNum=test.minNumberInRotateArray(list);
-        System.out.println(minNum);
+        int minNum1=test.minNumberInRotateArray(list);
+        System.out.println(minNum1);
+
+        int minNum2=test.minNumberInRotate(list,list.length);
+        System.out.println(minNum2);
 
     }
 }

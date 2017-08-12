4ea5c96b985cf09bf868bfefae1b3d94ef9cff13
Sy <370418846@qq.com>
2017-08-12 14:36:31 +0800

    剑指offer 第三章

--- src/offer03/ch03.java
+++ src/offer03/ch03.java
@@ -10,8 +10,18 @@ class ListNode{
         this.val = val;
     }
 }
+
+class TreeNode {
+ int val = 0;
+ TreeNode left = null;
+ TreeNode right = null;
+ public TreeNode(int val) {
+ this.val = val;
+ }
+ }
+
 public class ch03 {
-    //题目：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+    //题目2：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
     // 使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分，
     // 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
     public void reOrderArray(int [] array) {
@@ -40,7 +50,7 @@ public class ch03 {
 
 
 
-    //题目：输入一个链表，输出该链表中倒数第k个结点。方法一：强解法
+    //题目3：输入一个链表，输出该链表中倒数第k个结点。方法一：强解法
     public ListNode FindKthToTail(ListNode head,int k) {
         ListNode node=head;
         int count=1;
@@ -62,7 +72,7 @@ public class ch03 {
             return node;
         }
     }
-    //题目：输入一个链表，输出该链表中倒数第k个结点。方法一：用栈的先进后出
+    //题目3：输入一个链表，输出该链表中倒数第k个结点。方法二：用栈的先进后出
     public ListNode FindKthToTail_1(ListNode head,int k) {
         ListNode result = null;
         if(head == null || k <= 0){
@@ -82,30 +92,116 @@ public class ch03 {
         }
         return result;
     }
+    //题目4：输入一个链表，反转链表后，输出链表的所有元素。
+    //除了基本类型，class，赋等值的不同变量其实指向同一个内存
+    public ListNode ReverseList(ListNode head) {
+           ListNode preListNode=null;
+           ListNode nowListNode=head;
+           ListNode reverseNode=null;
+           while (nowListNode!=null){
+               ListNode nextListNode=nowListNode.next;
+               if(nextListNode==null)
+                   reverseNode=nowListNode;
+               nowListNode.next=preListNode;
+               preListNode=nowListNode;
+               nowListNode=nextListNode;
+            }
+           return reverseNode;
+    }
+    //题目5：输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则
+    //用递归来解。。。
+    public ListNode Merge(ListNode list1,ListNode list2) {
+          if(list1==null)
+              return list2;
+          else if (list2==null)
+            return list1;
+          ListNode mergeList=null;
+          if(list1.val<list2.val){
+              mergeList=list1;
+              mergeList.next=Merge(list1.next,list2);
+          }
+          else {
+              mergeList=list2;
+              mergeList.next=Merge(list1,list2.next);
+          }
+          return mergeList;
+
+    }
+    //题目：输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        boolean check=false;
+        if(root2!=null&&root1!=null)
+        {
+            if(root1.val==root2.val)
+                check=DoseTreeHasSubtree(root1,root2);
+            if(check==false)
+                check=HasSubtree(root1.left,root2);
+            if(check==false)
+                check=HasSubtree(root1.right,root2);
+        }
+        return check;
+    }
+    public boolean DoseTreeHasSubtree(TreeNode root1,TreeNode root2) {
+        if(root2==null)
+            return true;
+        if(root1==null)
+            return false;
+        if(root1.val!=root2.val)
+            return false;
+        return DoseTreeHasSubtree(root1.left,root2.left)&&DoseTreeHasSubtree(root1.right,root2.right);
+    }
 
 
     public static void main(String[] args){
-//        int[] array={1,2,3,4,5};
         ch03 test=new ch03();
-//        test.reOrderArray(array);
-        //题目：输入一个链表，输出该链表中倒数第k个结点。
-        ListNode node1=new ListNode(1);
-        ListNode node2=new ListNode(2);
-        ListNode node3=new ListNode(3);
-        ListNode node4=new ListNode(4);
-        ListNode node5=new ListNode(5);
-        node1.next=node2;
-        node2.next=node3;
-        node3.next=node4;
-        node4.next=node5;
 
-        int k=1;
-        //方法一
-        ListNode node=test.FindKthToTail(node1,k);
-        System.out.println(node.val);
-        //方法二
-        ListNode zz=test.FindKthToTail_1(node1,3);
-        System.out.println(zz.val);
+
+        //题2
+//        int[] array={1,2,3,4,5};
+//        test.reOrderArray(array);
+
+        //题目3~5用
+//        ListNode node1=new ListNode(1);
+//        ListNode node2=new ListNode(2);
+//        ListNode node3=new ListNode(3);
+//        ListNode node4=new ListNode(4);
+//        ListNode node5=new ListNode(5);
+//        node1.next=node2;
+//        node2.next=node3;
+//        node3.next=node4;
+//        node4.next=node5;
+        //题目3：输入一个链表，输出该链表中倒数第k个结点。
+//        int k=1;
+//        //方法一
+//        ListNode node=test.FindKthToTail(node1,k);
+//        System.out.println(node.val);
+//        //方法二
+//        ListNode zz=test.FindKthToTail_1(node1,3);
+//        System.out.println(zz.val)
+        //题4：
+//        ListNode node=test.ReverseList(node1);
+//        System.out.println(node.val);
+        //题5：
+//        ListNode node=test.Merge(node1,node4);
+//        System.out.println(node.val);
+        //题6：
+        TreeNode nodeRoot1=new TreeNode(1);
+        TreeNode node1=new TreeNode(2);
+        TreeNode node2=new TreeNode(3);
+        TreeNode node3=new TreeNode(5);
+        nodeRoot1.left=node1;
+        nodeRoot1.right=node2;
+        node1.right=node3;
+
+
+        TreeNode nodeRoot2=new TreeNode(2);
+        nodeRoot2.right=node3;
+        boolean check=test.HasSubtree(nodeRoot1,nodeRoot2);
+        System.out.println(check);
+
+
+
+
 
     }
 }
